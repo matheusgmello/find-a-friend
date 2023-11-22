7ff5f38c78f7e20f09f12368124fa710389bfa9c
Matheus Gabriel <matheusgmello1@gmail.com>
2023-11-22 15:24:13 -0300

    Feat: Migrations org e pets

---
+++ prisma/migrations/20231023194646_org_and_pet_model/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "orgs" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "whatsapp" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "endereco" TEXT NOT NULL,
+    "estado" TEXT NOT NULL,
+    "cidade" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "orgs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT NOT NULL,
+    "age" TEXT NOT NULL,
+    "size" TEXT NOT NULL,
+    "energy_level" TEXT NOT NULL,
+    "level_independence" TEXT NOT NULL,
+    "ambience" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "org_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_email_key" ON "orgs"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "orgs_whatsapp_key" ON "orgs"("whatsapp");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_org_id_fkey" FOREIGN KEY ("org_id") REFERENCES "orgs"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
