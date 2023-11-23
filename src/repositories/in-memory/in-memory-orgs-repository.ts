53d76ef887b80882fee624615f8b22d56d019239
Matheus Gabriel <matheusgmello1@gmail.com>
2023-11-23 10:31:34 -0300

    Feat: Atualizando interface e implementando em orgs

--- src/repositories/in-memory/in-memory-orgs-repository.ts
+++ src/repositories/in-memory/in-memory-orgs-repository.ts
@@ -5,6 +5,16 @@ import { randomUUID } from 'node:crypto'
 export class InMemoryOrgsRepository implements OrgsRepository {
   public items: Org[] = []
 
+  async findById(id: string) {
+    const org = this.items.find((item) => item.id === id)
+
+    if (!org) {
+      return null
+    }
+
+    return org
+  }
+
   async findByEmail(email: string) {
     const org = this.items.find((item) => item.email === email)
 
@@ -17,7 +27,7 @@ export class InMemoryOrgsRepository implements OrgsRepository {
 
   async create(data: Prisma.OrgCreateInput) {
     const org = {
-      id: randomUUID(),
+      id: data.id ?? randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
@@ -32,5 +42,13 @@ export class InMemoryOrgsRepository implements OrgsRepository {
     this.items.push(org)
 
     return org
+  }
+
+  async findManyByStateAndCity(data: { city: string; state: string }) {
+    const orgs = this.items.filter(
+      (org) => org.cidade === data.city && org.estado === data.state,
+    )
+
+    return orgs
   }
 }
--- src/repositories/orgs-repository.ts
+++ src/repositories/orgs-repository.ts
@@ -1,6 +1,8 @@
 import { Org, Prisma } from '@prisma/client'
 
 export interface OrgsRepository {
+  findById(id: string): Promise<Org | null>
   findByEmail(email: string): Promise<Org | null>
+  findManyByStateAndCity(data: { city: string; state: string }): Promise<Org[]>
   create(data: Prisma.OrgCreateInput): Promise<Org>
 }
