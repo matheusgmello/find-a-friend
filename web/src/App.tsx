c90506be0972e8b2d46e5544559af9a20cbd49b5
Matheus Gabriel <matheusgmello1@gmail.com>
2023-11-24 11:29:36 -0300

    Fix : app files

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,26 @@
+import { Router } from './routes'
+import { BrowserRouter } from 'react-router-dom'
 import './styles/global.css'
+import { LocationContextProvider } from './context/LocationContext'
+import { AlertContextProvider } from './context/AlertContext'
+import { UserContextProvider } from './context/UserContext'
+import { PetContextProvider } from './context/PetContext'
+import { OrgContextProvider } from './context/OrgContext'
 
 export function App() {
-  return 'hello friend!'
+  return (
+    <BrowserRouter>
+      <AlertContextProvider>
+        <LocationContextProvider>
+          <UserContextProvider>
+            <PetContextProvider>
+              <OrgContextProvider>
+                <Router />
+              </OrgContextProvider>
+            </PetContextProvider>
+          </UserContextProvider>
+        </LocationContextProvider>
+      </AlertContextProvider>
+    </BrowserRouter>
+  )
 }
